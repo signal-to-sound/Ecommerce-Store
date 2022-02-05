2fad3d2a406f7dfc0d4157b871d3bcd0f4eb6041
skydragon <bwalikurunda@gmail.com>
2022-02-05 15:27:59 +0100

    Connect line items, carts and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
 
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
 
     validates :title, :description, :image_url, presence: true
     
@@ -11,4 +14,15 @@ class Product < ApplicationRecord
         }
         
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    private
+
+        #ensure that there are no line items referencing the product
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
+    
 end
