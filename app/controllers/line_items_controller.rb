499d151440e8ee95a24e126eb6dce1ce9c33c104
skydragon <bwalikurunda@gmail.com>
2022-02-13 03:16:47 +0100

    Samrt cart, log errors, fix failing line-items controller tests

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,15 +24,16 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
    product = Product.find(params[:product_id])
-   @line_item = @cart.line_items.build(product: product)
+   @line_item = @cart.add_product(product)
+   #  @line_item = @cart.line_items.build(product: product)
  
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
+        format.html { redirect_to @line_item.cart  }
         format.json { render :show, status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +70,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
