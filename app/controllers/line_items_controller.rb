b6015ce1612534ce64dd565617b2ba36d3774c13
skydragon <bwalikurunda@gmail.com>
2022-02-17 02:51:18 +0100

    Cart partial within store page, fix failing test.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,6 +23,7 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
+    
    product = Product.find(params[:product_id])
    @line_item = @cart.add_product(product)
    #  @line_item = @cart.line_items.build(product: product)
@@ -30,7 +31,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart  }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
