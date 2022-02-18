c8ecffd33b08b5e26da734eea5c6515a21332e04
skydragon <bwalikurunda@gmail.com>
2022-02-18 14:46:43 +0100

    Add Ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,9 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.json { render :show, status: :created, location: @line_item }
+        format.js
+        # format.json { render :show, status: :created, location: @line_item }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
